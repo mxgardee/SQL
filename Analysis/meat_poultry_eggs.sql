b2989959152d72cdf5e7ab461d7b0b08a35a549a
Muhammed <mxgardeee@gmail.com>
2021-03-10 13:37:56 +0200

    seventh commit

---
+++ Analysis/meat_poultry_eggs.sql
@@ -0,0 +1,12 @@
+CREATE TABLE meat_poultry_egg_inspect (
+    est_number varchar(50) CONSTRAINT est_number_key PRIMARY KEY,
+    company varchar(100),
+    street varchar(100),
+    city varchar(30),
+    st varchar(2),
+    zip varchar(5),
+    phone varchar(14),
+    grant_date date,
+    activities text,
+    dbas text
+);
