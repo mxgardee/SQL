abd0e43129b84fc5a1dbe01df60eb719800b8cf5
Muhammed <mxgardeee@gmail.com>
2021-03-10 14:26:42 +0200

    eighth commit

--- Analysis/meat_poultry_eggs.sql
+++ Analysis/meat_poultry_eggs.sql
@@ -1,12 +1,34 @@
-CREATE TABLE meat_poultry_egg_inspect (
-    est_number varchar(50) CONSTRAINT est_number_key PRIMARY KEY,
-    company varchar(100),
-    street varchar(100),
-    city varchar(30),
-    st varchar(2),
-    zip varchar(5),
-    phone varchar(14),
-    grant_date date,
-    activities text,
-    dbas text
-);
+
+
+CREATE TABLE meat_poultry_egg_inspect_backup AS
+SELECT * FROM meat_poultry_egg_inspect;
+
+ALTER TABLE meat_poultry_egg_inspect ADD COLUMN st_copy varchar(2);
+UPDATE meat_poultry_egg_inspect
+SET st_copy = st;
+
+SELECT st,
+st_copy
+FROM meat_poultry_egg_inspect
+ORDER BY st;
+
+UPDATE meat_poultry_egg_inspect
+SET st = 'MN'
+WHERE est_number = 'V18677A';
+UPDATE meat_poultry_egg_inspect
+SET st = 'AL'
+WHERE est_number = 'M45319+P45319';
+UPDATE meat_poultry_egg_inspect
+SET st = 'WI'
+WHERE est_number = 'M263A+P263A+V263A';
+
+UPDATE meat_poultry_egg_inspect
+SET st = st_copy;
+UPDATE meat_poultry_egg_inspect original
+SET st = backup.st
+FROM meat_poultry_egg_inspect_backup backup
+WHERE original.est_number = backup.est_number;
+
+
+
+
