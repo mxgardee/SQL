7a9442e6308a323a34bb506376981ec1f118a819
Muhammed <mxgardeee@gmail.com>
2021-03-10 14:59:27 +0200

    ninth commit

--- Analysis/meat_poultry_eggs.sql
+++ Analysis/meat_poultry_eggs.sql
@@ -29,6 +29,56 @@ SET st = backup.st
 FROM meat_poultry_egg_inspect_backup backup
 WHERE original.est_number = backup.est_number;
 
+SELECT st,
+st_copy
+FROM meat_poultry_egg_inspect
+ORDER BY st;
+
+UPDATE meat_poultry_egg_inspect
+SET st = 'MN'
+WHERE est_number = 'V18677A';
+UPDATE meat_poultry_egg_inspect
+SET st = 'AL'
+WHERE est_number = 'M45319+P45319';
+UPDATE meat_poultry_egg_inspect
+SET st = 'WI'
+WHERE est_number = 'M263A+P263A+V263A';
+
+UPDATE meat_poultry_egg_inspect
+SET st = st_copy;
+
+UPDATE meat_poultry_egg_inspect original
+SET st = backup.st
+FROM meat_poultry_egg_inspect_backup backup
+WHERE original.est_number = backup.est_number;
+
+ALTER TABLE meat_poultry_egg_inspect ADD COLUMN company_standard varchar(100);
+UPDATE meat_poultry_egg_inspect
+SET company_standard = company;
+
+UPDATE meat_poultry_egg_inspect
+SET company_standard = 'Armour-Eckrich Meats'
+WHERE company LIKE 'Armour%';
+
+SELECT company, company_standard
+FROM meat_poultry_egg_inspect
+WHERE company LIKE 'Armour%';
+
+ALTER TABLE meat_poultry_egg_inspect ADD COLUMN zip_copy varchar(5);
+UPDATE meat_poultry_egg_inspect
+SET zip_copy = zip;
+
+UPDATE meat_poultry_egg_inspect
+SET zip = '00' || zip
+WHERE st IN('PR','VI') AND length(zip) = 3;
+
+UPDATE meat_poultry_egg_inspect
+SET zip = '0' || zip
+WHERE st IN('CT','MA','ME','NH','NJ','RI','VT') AND length(zip) = 4;
+
+
+
+
 
 
 
